fed483ec2ee8ccf9ed413e3ef6e8bbe658dec94b
MosQyy <akbarma2002@gmail.com>
2022-06-15 22:23:06 +0700

    add touch id biometric

--- android/app/src/main/java/com/secondhand/MainApplication.java
+++ android/app/src/main/java/com/secondhand/MainApplication.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.rnfingerprint.FingerprintAuthPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
