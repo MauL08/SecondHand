a964b8a74c3addbc0b32b658a5de6cc1a70fe97b
MosQyy <akbarma2002@gmail.com>
2022-06-16 01:24:55 +0700

    config notification

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import RootNavigator from './core/router';
 import { Provider } from 'react-redux';
 import SplashScreen from 'react-native-splash-screen';
 import CodePush from 'react-native-code-push';
+import PushNotification from 'react-native-push-notification';
 
 import { persistor, store } from './data/store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -13,10 +14,18 @@ const codePushOptions = {
 
 const App = () => {
   useEffect(() => {
+    createChannels();
     setTimeout(() => {
       SplashScreen.hide();
     }, 2000);
   }, []);
+
+  const createChannels = () => {
+    PushNotification.createChannel({
+      channelId: 'App-Channel',
+      channelName: 'App Channel',
+    });
+  };
 
   return (
     <Provider store={store}>
