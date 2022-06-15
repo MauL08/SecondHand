ef94be437b2e984f455286e4bdeaca7d5aca568a
MosQyy <akbarma2002@gmail.com>
2022-06-15 17:58:08 +0700

    config eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parser: '@babel/eslint-parser',
+  rules: {
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: false,
+      },
+    ],
+  },
+  env: {
+    'jest/globals': true,
+  },
 };
