e88e8315dd3d69066a504eae5e9e19d3beb35a4b
MosQyy <akbarma2002@gmail.com>
2022-06-16 01:21:14 +0700

    add notification config

--- android/app/src/main/java/com/secondhand/MainApplication.java
+++ android/app/src/main/java/com/secondhand/MainApplication.java
@@ -3,6 +3,7 @@ package com.secondhand;
 import android.app.Application;
 import android.content.Context;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.imagepicker.ImagePickerPackage;
