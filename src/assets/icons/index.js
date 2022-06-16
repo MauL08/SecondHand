a3563402f9b275f39ed761197ecfbac67772e3de
akdeaso <akdeaso@gmail.com>
2022-06-16 20:41:58 +0700

    add register screen

---
+++ src/assets/icons/index.js
@@ -0,0 +1,35 @@
+import ArrowLeft from './icon_arrow-left.png';
+import EyeOff from './icon_eye-off.png';
+import Eye from './icon_eye.png';
+import Search from './icon_search.png';
+import Camera from './icon_camera.png';
+import Whatsapp from './icon_whatsapp.png';
+import Dollar from './icon_dollar-sign.png';
+import Heart from './icon_heart.png';
+import Plus from './icon_plus.png';
+import ChevronDown from './icon_chevron-down.png';
+import List from './icon_list.png';
+import PlusCirlce from './icon_plus-circle.png';
+import Bell from './icon_bell.png';
+import Home from './icon_home.png';
+import User from './icon_user.png';
+import Box from './icon_box.png';
+
+export const Icons = {
+  ArrowLeft,
+  EyeOff,
+  Eye,
+  Search,
+  Camera,
+  Whatsapp,
+  Dollar,
+  Heart,
+  Plus,
+  ChevronDown,
+  List,
+  PlusCirlce,
+  Bell,
+  Home,
+  User,
+  Box,
+};
