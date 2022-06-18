f6071f3393b318209e7c66dbd84950681c99fdf0
MosQyy <akbarma2002@gmail.com>
2022-06-18 13:05:57 +0700

    add template for slices

---
+++ src/data/slices/notificationSlice.js
@@ -0,0 +1,24 @@
+// import { Alert } from 'react-native';
+import axios from 'axios';
+// import { baseURL } from '../baseAPI';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import { navigate } from '../../core/router/navigator';
+// import { setLoading } from './globalSlice';
+
+axios.defaults.validateStatus = status => {
+  return status < 500;
+};
+
+export const test = createAsyncThunk();
+
+const initialState = {
+  data: {},
+};
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState,
+  extraReducers: {},
+});
+
+export default notificationSlice.reducer;
