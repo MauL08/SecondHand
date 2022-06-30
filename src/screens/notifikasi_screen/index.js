e8fab694c5d78d16c1a6082cb055dc201c62cf15
akdeaso <akdeaso@gmail.com>
2022-06-30 18:48:12 +0700

    add screen notifikasi

--- src/screens/notifikasi_screen/index.js
+++ src/screens/notifikasi_screen/index.js
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { COLORS } from '../../assets/colors';
+import { ms } from 'react-native-size-matters';
 
 const NotifikasiScreen = () => {
   return (
-    <View>
-      <Text>NotifikasiScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.screenTitle}>Notifikasi</Text>
     </View>
   );
 };
 
 export default NotifikasiScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.neutral1,
+    padding: ms(16),
+  },
+  screenTitle: {
+    fontSize: ms(20),
+    lineHeight: ms(30),
+    fontFamily: 'Poppins-Bold',
+    color: COLORS.black,
+  },
+});
