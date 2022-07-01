d48d8e536a101bc57003e8de94fc57dd7e089a25
akdeaso <akdeaso@gmail.com>
2022-07-01 14:00:29 +0700

    done conditional render

--- src/screens/notifikasi_screen/index.js
+++ src/screens/notifikasi_screen/index.js
@@ -15,33 +15,53 @@ const NotifikasiScreen = () => {
     {
       id: 1,
       name: 'Jam Tangan Casio',
-      price: 'Rp 250.000',
-      ditawar: 'Rp 200.000',
-      timestamp: '19 Apr, 12:00',
+      product_price: 'Rp 250.000',
+      bid_price: 'Rp 200.000',
+      status: 'bid',
+      transaction_date: '19 Apr, 12:00',
       image_url:
         'https://www.utileincasa.it/wp-content/uploads/2021/10/6oM-lgd-olia-nayda-dB3pkARCxHI-unsplash-scaled-e1629735546986.jpg',
     },
     {
       id: 2,
       name: 'Jam Tangan Casio',
-      price: 'Rp 250.000',
-      ditawar: 'Rp 200.000',
-      timestamp: '19 Apr, 12:00',
+      product_price: 'Rp 250.000',
+      bid_price: '',
+      status: 'terbit',
+      transaction_date: '19 Apr, 12:00',
       image_url:
         'https://www.utileincasa.it/wp-content/uploads/2021/10/6oM-lgd-olia-nayda-dB3pkARCxHI-unsplash-scaled-e1629735546986.jpg',
     },
     {
       id: 3,
       name: 'Jam Tangan Casio',
-      price: 'Rp 250.000',
-      ditawar: 'Rp 200.000',
-      timestamp: '19 Apr, 12:00',
+      product_price: 'Rp 250.000',
+      bid_price: 'Rp 200.000',
+      status: 'accepted',
+      transaction_date: '19 Apr, 12:00',
+      image_url:
+        'https://www.utileincasa.it/wp-content/uploads/2021/10/6oM-lgd-olia-nayda-dB3pkARCxHI-unsplash-scaled-e1629735546986.jpg',
+    },
+    {
+      id: 4,
+      name: 'Jam Tangan Casio',
+      product_price: 'Rp 250.000',
+      bid_price: 'Rp 200.000',
+      status: 'declined',
+      transaction_date: '19 Apr, 12:00',
       image_url:
         'https://www.utileincasa.it/wp-content/uploads/2021/10/6oM-lgd-olia-nayda-dB3pkARCxHI-unsplash-scaled-e1629735546986.jpg',
     },
   ];
 
-  const NotifCard = ({ name, price, ditawar, timestamp, image_url }) => {
+  const NotifCard = ({
+    name,
+    product_price,
+    bid_price,
+    status,
+    transaction_date,
+    image_url,
+  }) => {
     return (
       <TouchableOpacity style={styles.cardContainer}>
         <View style={styles.row}>
@@ -53,17 +73,48 @@ const NotifikasiScreen = () => {
           />
           <View style={styles.textContainer}>
             <View style={styles.regularContainer}>
-              <Text style={styles.regularSubText}>Penawaran produk</Text>
+              <Text style={styles.regularSubText}>
+                {' '}
+                {status === 'terbit'
+                  ? 'Berhasil diterbitkan'
+                  : 'Penawaran produk'}
+              </Text>
               <View style={styles.timestampContainer}>
-                <Text style={styles.regularSubText}>{timestamp}</Text>
+                <Text style={styles.regularSubText}>{transaction_date}</Text>
                 <View style={styles.circle}></View>
               </View>
             </View>
             <Text style={styles.regularText2}>{name}</Text>
-            <Text style={styles.regularText2}>{price}</Text>
-            <Text style={styles.regularText2}>Ditawar {ditawar}</Text>
+            <Text
+              style={[
+                styles.regularText2,
+                {
+                  textDecorationLine:
+                    status === 'accepted' ? 'line-through' : null,
+                },
+              ]}>
+              {product_price}
+            </Text>
+            {status === 'bid' ? (
+              <Text style={styles.regularText2}>Ditawar {bid_price}</Text>
+            ) : status === 'accepted' ? (
+              <Text style={styles.regularText2}>
+                Berhasil ditawar {bid_price}
+              </Text>
+            ) : status === 'declined' ? (
+              <Text style={styles.regularText2}>Gagal ditawar {bid_price}</Text>
+            ) : null}
           </View>
         </View>
+        {status === 'accepted' ? (
+          <Text style={styles.bottomSubText}>
+            Kamu akan segera dihubungi penjual via whatsapp
+          </Text>
+        ) : status === 'declined' ? (
+          <Text style={styles.bottomSubText}>
+            Penawaran ditolak oleh penjual
+          </Text>
+        ) : null}
       </TouchableOpacity>
     );
   };
@@ -78,14 +129,16 @@ const NotifikasiScreen = () => {
           renderItem={({ item }) => (
             <NotifCard
               name={item.name}
-              price={item.price}
+              product_price={item.product_price}
               image_url={item.image_url}
-              ditawar={item.ditawar}
-              timestamp={item.timestamp}
+              bid_price={item.bid_price}
+              transaction_date={item.transaction_date}
+              status={item.status}
             />
           )}
           keyExtractor={item => item.id}
           showsVerticalScrollIndicator={false}
+          style={styles.flatlistStyle}
         />
       </View>
     </View>
@@ -114,6 +167,8 @@ const styles = StyleSheet.create({
   },
   row: {
     flexDirection: 'row',
+  },
+  cardContainer: {
     borderBottomWidth: 1,
     borderBottomColor: COLORS.neutral01,
     paddingBottom: ms(16),
@@ -136,6 +191,7 @@ const styles = StyleSheet.create({
     color: COLORS.neutral4,
     lineHeight: ms(20),
     fontFamily: 'Poppins-Regular',
+    marginTop: ms(4),
   },
   textContainer: {
     marginLeft: ms(16),
@@ -147,7 +203,8 @@ const styles = StyleSheet.create({
   },
   timestampContainer: {
     flexDirection: 'row',
-    left: ms(108),
+    left: ms(206),
+    position: 'absolute',
   },
   circle: {
     width: ms(8),
@@ -156,5 +213,16 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.alertDanger,
     alignSelf: 'center',
     marginLeft: ms(8),
+  },
+  bottomSubText: {
+    fontSize: ms(10),
+    color: COLORS.neutral3,
+    lineHeight: ms(14),
+    fontFamily: 'Poppins-Regular',
+    marginLeft: ms(64),
+    marginTop: ms(4),
+  },
+  flatlistStyle: {
+    marginBottom: ms(16),
   },
 });
